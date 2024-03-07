603f82935e72b1e01fd65fbc2425729c8ad013a1
Aarshan24 <141824322+aarshan24@users.noreply.github.com>
2024-03-07 18:18:21 +0545

    Update get_info.py

--- get_info.py
+++ get_info.py
@@ -1,10 +1,13 @@
+from flask import Flask
 import subprocess
 
-# Get IP address
-ip_result = subprocess.run(["ip", "addr", "show", "eth0"], capture_output=True, text=True)
-ip_address = ip_result.stdout.split("inet ")[1].split("/")[0].strip()
+app = Flask(__name__)
 
-# Print information
-print(f"IP: {ip_address}")
-print("Username: root")
-print("Password: root")
+@app.route('/')
+def get_info():
+    ip_result = subprocess.run(["ip", "addr", "show", "eth0"], capture_output=True, text=True)
+    ip_address = ip_result.stdout.split("inet ")[1].split("/")[0].strip()
+    return f"IP: {ip_address}<br>Username: root<br>Password: root"
+
+if __name__ == '__main__':
+    app.run()
